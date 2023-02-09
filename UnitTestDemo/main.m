e0843f22c83f5f4f9860b969bc3ea0e0ebd6f5ff
Archie <youkun@bilibili.com>
2023-02-09 14:36:48 +0800

    Initial Commit

---
+++ UnitTestDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UnitTestDemo
+//
+//  Created by 尤坤 on 2023/2/9.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
